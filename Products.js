21d500d286eae176b06c6c5de24889cb653f052e
jbmarquez27 <jbmarquez_27@icloud.com>
2026-05-11 22:50:33 +0800

    Update Products.js

--- Products.js
+++ Products.js
@@ -61,7 +61,7 @@ const products = [
 
 { 
     name:"Pokemon TCG [ME05] Pitch Black Half Booster",
-    img: "Half Booster.png",
+    img: "half booster.png",
     unitPrice: 4300,
     casePrice: 51600,
     unitDP: 1290,
